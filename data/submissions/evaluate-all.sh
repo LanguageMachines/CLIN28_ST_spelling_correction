dbc30bd33dd871e908a6a730de8b19e08d3f777a
Maarten van Gompel <proycon@anaproy.nl>
2018-01-15 23:26:33 +0100

    adding evaluation wrapper script

---
+++ data/submissions/evaluate-all.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for d in */; do
+    for f in $d/*.json; do
+        nr=$(basename $d)
+        filename=$(basename $f)
+        mkdir -p evaluation/$nr 2>/dev/null
+        clin28-evaluate --out $f --ref ../goldstandard/$filename > evaluation/$nr/$filename 2>evaluation/$nr/${filename/json/log}
+    done
+done
+
