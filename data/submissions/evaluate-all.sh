1eed0497ec1af1a563893874fec922e5ee9d1058
Maarten van Gompel <proycon@anaproy.nl>
2018-01-16 12:57:40 +0100

    evaluation script fix

--- data/submissions/evaluate-all.sh
+++ data/submissions/evaluate-all.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
-for d in ?/; do
-    for f in $d/*.json; do
-        nr=$(basename $d)
-        filename=$(basename $f)
-        mkdir -p evaluation/$nr 2>/dev/null
-        clin28-evaluate --out $f --ref ../goldstandard/$filename > evaluation/$nr/$filename 2>evaluation/$nr/${filename/json/log}
+for f in ../goldstandard/*.json; do
+    filename=$(basename $f)
+    echo "Evaluating $filename">&2
+    for d in ?/; do
+        if [ -f $d/$filename ]; then
+            nr=$(basename $d)
+            echo $nr>&2
+            mkdir -p evaluation/$nr 2>/dev/null
+            clin28-evaluate --out $d/$filename --ref ../goldstandard/$filename > evaluation/$nr/$filename 2>evaluation/$nr/${filename/json/log}
+            if [ $? -ne 0 ]; then
+                echo "Failed, check: evaluation/$nr/${filename/json/log}">&2
+                exit 2
+            fi
+        else
+            echo "MISSING OUTPUT FOR CANDIDATE $nr: $filename">&2
+            exit 2
+        fi
     done
 done
 
+echo "Computing result summary...">&2
 python3 resultsummary.py
 
